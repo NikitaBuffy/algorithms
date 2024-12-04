9745aa1a4c9822757da1c4e43ca3e962a6382990
NikitaBuffy <pominov.nk@gmail.com>
2024-12-04 11:51:31 +0300

    leetcode #155 min stack

---
+++ src/main/java/leetcode/MinStack.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * LeetCode #155
+ */
+public class MinStack {
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top());    // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+
+    private final Stack<Integer> stack;
+    private final Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
