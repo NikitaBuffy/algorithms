75f7cbd00094b9e6028ace4439ed280ecb13c6c4
NikitaBuffy <pominov.nk@gmail.com>
2025-02-21 13:37:36 +0300

    leetcode #92 reverse linked list 2

---
+++ src/main/java/leetcode/ReverseLinkedList2.java
@@ -0,0 +1,62 @@
+package leetcode;
+
+/**
+ * LeetCode #92
+ */
+public class ReverseLinkedList2 {
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println(Solution.reverseBetween(node, 2, 4));
+        System.out.println(Solution.reverseBetween(new ListNode(5), 1, 1));
+        System.out.println(Solution.reverseBetween(new ListNode(3, new ListNode(5)), 1, 1));
+        System.out.println(Solution.reverseBetween(new ListNode(1, new ListNode(2, new ListNode(3))), 3, 3));
+    }
+
+    static class Solution {
+        public static ListNode reverseBetween(ListNode head, int left, int right) {
+            if (head.next == null || left == right) {
+                return head;
+            }
+
+            ListNode dummy = new ListNode(0, head);
+            ListNode prev = dummy;
+
+            for (int i = 0; i < left - 1; i++) {
+                prev = prev.next;
+            }
+
+            ListNode curr = prev.next;
+            for (int i = 0; i < right - left; i++) {
+                ListNode temp = curr.next;
+                curr.next = temp.next;
+                temp.next = prev.next;
+                prev.next = temp;
+            }
+
+            return dummy.next;
+        }
+    }
+
+    static class ListNode {
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+
+        int val;
+        ListNode next;
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+}
