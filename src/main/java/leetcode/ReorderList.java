12d4f98576cc44e3b7a48d4daab31da02aedcafb
NikitaBuffy <pominov.nk@gmail.com>
2025-02-27 15:31:44 +0300

    leetcode #143 reorder list

---
+++ src/main/java/leetcode/ReorderList.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import util.ListNode;
+
+/**
+ * LeetCode #143
+ */
+public class ReorderList {
+    public static void main(String[] args) {
+        System.out.println(Solution.reorderList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))))));
+        System.out.println(Solution.reorderList(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))));
+    }
+
+    static class Solution {
+        public static ListNode reorderList(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head.next;
+
+            // Split linked list into two parts by slow and fast pointers (finding the middle)
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            ListNode second = slow.next;
+            ListNode prev = null;
+            slow.next = null;
+
+            // Reversing the second part
+            while (second != null) {
+                ListNode next = second.next;
+                second.next = prev;
+                prev = second;
+                second = next;
+            }
+
+            ListNode first = head;
+            second = prev;
+
+            // Merging two parts
+            while (second != null) {
+                ListNode tmp1 = first.next;
+                ListNode tmp2 = second.next;
+                first.next = second;
+                second.next = tmp1;
+                second = tmp2;
+                first = tmp1;
+            }
+
+            return head;
+        }
+    }
+}
