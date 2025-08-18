50a6624ef79828d7a733f368baea32bb089cd83e
NikitaBuffy <pominov.nk@gmail.com>
2025-08-18 11:27:03 +0300

    leetcode #49 group anagrams optimized solution

--- src/main/java/leetcode/hashtable/GroupAnagrams.java
+++ src/main/java/leetcode/hashtable/GroupAnagrams.java
@@ -12,12 +12,14 @@ public class GroupAnagrams {
         String[] case2 = {""};
         String[] case3 = {"a"};
 
-        System.out.println(Solution.groupAnagrams(case1));
-        System.out.println(Solution.groupAnagrams(case2));
-        System.out.println(Solution.groupAnagrams(case3));
+        System.out.println(Solution.groupAnagramsOptimized(case1));
+        System.out.println(Solution.groupAnagramsOptimized(case2));
+        System.out.println(Solution.groupAnagramsOptimized(case3));
     }
 
     public static class Solution {
+
+        // O(n * k log k)
         public static List<List<String>> groupAnagrams(String[] strs) {
             if (strs.length == 0) {
                 return new ArrayList<>();
@@ -29,6 +31,26 @@ public class GroupAnagrams {
                 char[] chars = s.toCharArray();
                 Arrays.sort(chars);
                 String key = new String(chars);
+                map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            }
+
+            return new ArrayList<>(map.values());
+        }
+
+        // O(n * k)
+        public static List<List<String>> groupAnagramsOptimized(String[] strs) {
+            if (strs.length == 0) {
+                return new ArrayList<>();
+            }
+
+            Map<String, List<String>> map = new HashMap<>();
+
+            for (String s : strs) {
+                int[] count = new int[26];
+                for (char c : s.toCharArray()) {
+                    count[c - 'a']++;
+                }
+                String key = Arrays.toString(count);
                 map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
             }
 
