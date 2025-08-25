b15399dde3916213628cd4bfae075d763ded4075
NikitaBuffy <pominov.nk@gmail.com>
2025-08-25 14:30:54 +0300

    leetcode #14 longest common prefix optimized

--- src/main/java/leetcode/LongestCommonPrefix.java
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.List;
+
 /**
  * LeetCode #14
  */
@@ -7,9 +9,11 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         System.out.println(longestCommonPrefix(new String[]{"flower","flow","flight"}));
         System.out.println(longestCommonPrefix(new String[]{"dog","racecar","car"}));
+        System.out.println(longestCommonPrefixOptimized(List.of("flower","flow","flight")));
+        System.out.println(longestCommonPrefixOptimized(List.of("dog","racecar","car")));
     }
 
-    // O(N * M)
+    // O(N * L^2)
     public static String longestCommonPrefix(String[] strs) {
         String firstWord = strs[0];
 
@@ -23,4 +27,27 @@ public class LongestCommonPrefix {
 
         return firstWord;
     }
+
+    // O(N * M)
+    public static String longestCommonPrefixOptimized(List<String> strs) {
+        // Определяем минимальную длину строки среди всех
+        int minLen = strs.get(0).length();
+        for (String el : strs) {
+            minLen = Math.min(minLen, el.length());
+        }
+
+        // Проверяем совпадение символов на каждой позиции до minLen
+        for (int i = 0; i < minLen; i++) {
+            char ch = strs.get(0).charAt(i);
+            for (String el : strs) {
+                if (el.charAt(i) != ch) {
+                    return el.substring(0, i);
+                }
+            }
+        }
+
+        // Все символы совпадают на отрезке длины minLen
+        return strs.get(0).substring(0, minLen);
+    }
 }
+
